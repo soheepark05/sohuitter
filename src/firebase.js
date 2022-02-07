6d096dc4ce59fba94880edb5e27373330105c799
soheepark05 <psh960505@gmail.com>
2022-02-08 01:01:24 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcBsT3TrCMsVGh_C3fet-xORoJLFyquZU",
+    authDomain: "sohuitter.firebaseapp.com",
+    projectId: "sohuitter",
+    storageBucket: "sohuitter.appspot.com",
+    messagingSenderId: "87935356890",
+    appId: "1:87935356890:web:c7afa3dc9bdf4bceed3ae3"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
